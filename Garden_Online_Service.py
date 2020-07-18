ff24245c7fd162c45b94ed2b8784d40b574f27a8
raihanulkarim5 <raihanulkarim5@gmail.com>
2020-07-18 10:37:43 +0600

    Task 2 (Functions Added)

--- Garden_Online_Service.py
+++ Garden_Online_Service.py
@@ -18,18 +18,31 @@ class Flowers:
         self.lobelia = l
         self.petunias = p
         self.total_packs = b + g + l + p
-        self.total_plants = b * Flowers.flowers.get('Begonias') + g * Flowers.flowers.get('Geraniums') + l * Flowers.flowers.get('Lobelia') + p * Flowers.flowers.get('Petunias')
-        self.area_req = (b * Flowers.area.get('Begonias') + g * Flowers.area.get('Geraniums') + l * Flowers.area.get('Lobelia') + p * Flowers.area.get('Petunias') ) / 1000
+
+    def total_plants(self):
+        plants = (self.begonius * Flowers.flowers.get( 'Begonias' ) + self.geraniums * Flowers.flowers.get(
+            'Geraniums' ) + self.lobelia * Flowers.flowers.get( 'Lobelia' ) + self.petunias * Flowers.flowers.get(
+            'Petunias' ))
+        return plants
+
+    def areas(self):
+        area_req = (self.begonius * Flowers.area.get( 'Begonias' ) + self.geraniums * Flowers.area.get(
+            'Geraniums' ) + self.lobelia * Flowers.area.get( 'Lobelia' ) + self.petunias * Flowers.area.get(
+            'Petunias' )) / 1000
+        return area_req
+
+    def display(self):
+        print( "Thank you for your order, you have ordered..." )
+        print( "{} packs of plants or {} individual plants.".format( self.total_packs, self.total_plants()))
+        print( "You have enough plant to cover {} square metres.".format( self.areas()))
 
 
-print("Welcome to Bingley Garden Centre Delivery Service..")
-print("How many packs of the following plants would you like?")
-begonias = int(input("Begonias (6 pack):"))
-geraniums = int(input("geraniums (6 pack):"))
-lobelia = int(input("Lobelia (9 pack):"))
-petunias = int(input("Petunias (9 pack):"))
+print( "Welcome to Bingley Garden Centre Delivery Service.." )
+print( "How many packs of the following plants would you like?" )
+begonias = int( input( "Begonias (6 pack):" ) )
+geraniums = int( input( "geraniums (6 pack):" ) )
+lobelia = int( input( "Lobelia (9 pack):" ) )
+petunias = int( input( "Petunias (9 pack):" ) )
 
-customer = Flowers(begonias, geraniums, lobelia, petunias)
-print("Thank you for your order, you have ordered...")
-print("{} packs of plants or {} individual plants.".format(customer.total_packs, customer.total_plants))
-print("You have enough plant to cover {} square metres." .format(customer.area_req))
+customer = Flowers( begonias, geraniums, lobelia, petunias )
+customer.display()
